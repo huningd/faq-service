451989d66065bded8218f959b7ab9337ff0a1836
huningd <flipdeskript@gmail.com>
2016-01-14 07:04:28 +0100

    Added typings because of problems with tsd behind a proxy

---
+++ faq-service-app/typings/tsd.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="alertify/alertify.d.ts" />
+/// <reference path="angularjs/angular-mocks.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="systemjs/systemjs.d.ts" />
